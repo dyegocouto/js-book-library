60227f22d8f9435b39f4ba3ab0f396437bab187c
dyegocouto <175648963+dyegocouto@users.noreply.github.com>
2026-03-15 12:44:26 -0300

    feat: create modal for adding a book
    Clicking the new book button brings up a modal now. It doesn't work
    yet.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const newBookButton = document.querySelector(".add-button");
+const modal = document.querySelector(".modal");
+
+newBookButton.addEventListener("click", () => {
+  modal.showModal();
+});
